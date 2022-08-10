4ffe7e604dbfcdacfc1a93981533acf13cbe57ae
Mcpy <593863655@qq.com>
2022-08-10 01:46:31 +0100

    添加项目文件。

---
+++ Project1/ThreadPool.cpp
@@ -0,0 +1,202 @@
+#include "ThreadPool.h"
+
+
+void ThreadTask::start()
+{
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+	std::cout << "This is a ThreadTask." << std::endl;
+}
+
+
+ThreadPool::ThreadPool(int core_pool_size, int max_pool_size, int buffer_size, int keep_alive_seconds)
+	:core_pool_size(core_pool_size), max_pool_size(max_pool_size), buffer_size(buffer_size), keep_alive_seconds(keep_alive_seconds), termination_flag(0), running_num(0),
+	last_running_num(0), clear_flag(0), need_clear_num(0), timestamp(std::chrono::high_resolution_clock::now()), threadpool_management(&ThreadPool::threadpoolManagement, this)
+{
+	for (int i = 0; i < core_pool_size; i++)
+	{
+		thread_pool.emplace_back(&ThreadPool::work, this);
+	}
+}
+
+ThreadPool::~ThreadPool()
+{
+	termination_flag = 1;
+	cv_management.notify_one();
+	if (threadpool_management.joinable())
+		threadpool_management.join();
+	cv_thread_pool.notify_all();
+	for (auto& i : thread_pool)
+	{
+		if (i.joinable())
+		{
+			i.join();
+		}
+	}
+
+}
+
+int ThreadPool::poolSize()
+{
+	return thread_pool.size();
+}
+
+int ThreadPool::bufferSize()
+{
+	return task_buffer.size();
+}
+
+void ThreadPool::pushTask(ThreadTask* task)
+{
+	std::unique_lock<std::mutex> ulock(mtx);
+	task_buffer.push(task);
+	ulock.unlock();
+	cv_management.notify_one();
+}
+
+void ThreadPool::work()
+{
+	while (!termination_flag)
+	{
+		std::unique_lock<std::mutex> ulock(mtx);
+		while (task_buffer.empty() && !clear_flag && !termination_flag)
+		{
+			cv_thread_pool.wait(ulock);
+		}
+		if (termination_flag)
+			break;
+		//清理标记，结束本线程
+		if (clear_flag)
+		{
+			if (need_clear_num > 0)
+			{
+				ulock.unlock();
+				need_clear_num--;
+				std::unique_lock<std::mutex> ulock_clear_queue(mtx_clear_queue);
+				clear_thread_id.push(std::this_thread::get_id());
+				break;
+			}
+			else
+			{
+				clear_flag = 0;
+			}
+		}
+		ThreadTask* task = task_buffer.front();
+		task_buffer.pop();
+		ulock.unlock();
+		if (task != nullptr)
+		{
+			running_num++;
+			try
+			{
+				task->start();
+			}
+			//线程任务的异常处理，请结合项目要求自己添加
+			catch (const char* msg)
+			{
+				////////////////////////////
+			}
+			catch (std::exception& e)
+			{
+				const char* msg = e.what();
+				/////////////////////////////
+			}
+			catch (...)
+			{
+				///////////////////////
+			}
+			running_num--;
+		}
+	}
+}
+
+void ThreadPool::threadpoolManagement()
+{
+	while (!termination_flag)
+	{
+		std::unique_lock<std::mutex> ulock(mtx);
+		while (task_buffer.empty() && thread_pool.size() <= core_pool_size && !termination_flag)
+		{
+			cv_management.wait(ulock);
+		}
+		if (termination_flag)
+			break;
+		int task_num = task_buffer.size();
+		ulock.unlock();
+		int free_thread = thread_pool.size() - running_num;
+		//如果任务量超过了任务缓存则需要添加线程
+		if (task_num - free_thread > buffer_size)
+		{
+			int add_num = task_num-free_thread;
+			//添加数量不得超过最大线程数
+			if (add_num + thread_pool.size() > max_pool_size)
+			{
+				add_num = max_pool_size - thread_pool.size();
+			}
+			for (int i = 0; i < add_num; i++)
+			{
+				thread_pool.emplace_back(&ThreadPool::work, this);
+			}
+			timestamp = std::chrono::high_resolution_clock::now();
+			last_running_num = running_num;
+		}
+		//如果有任务缓存则唤醒相应数量的线程
+		for (int i = 0; i < task_num; i++)
+		{
+			cv_thread_pool.notify_one();
+		}
+		//如果当前的线程数大于了常驻线程数量则需要处理多余的线程
+		if (thread_pool.size() > core_pool_size)
+		{
+			//每keep_alive_seconds检测一次
+			if (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - timestamp).count() > keep_alive_seconds)
+			{
+				timestamp = std::chrono::high_resolution_clock::now();
+				//当正在工作的线程较上个时间段少，可认为目前负载在减小，可清除多余且空闲的线程
+				//也可改为 last_running_num - running_num > a a为一个阈值
+				if (running_num < last_running_num)
+				{
+					int free_thread = thread_pool.size() - running_num, extra_thread = thread_pool.size() - core_pool_size;
+					need_clear_num = free_thread < extra_thread ? free_thread : extra_thread;
+					last_running_num = running_num;
+					if (need_clear_num > 0)
+					{
+						clear_flag = 1;
+						//唤醒需要清理的线程数量
+						for (int i = 0; i < need_clear_num; i++)
+						{
+							cv_thread_pool.notify_one();
+						}
+					}
+				}
+			}
+			std::this_thread::yield();
+			//删除已经结束的线程
+			while (1)
+			{
+				std::thread::id thread_id;
+				std::unique_lock<std::mutex> ulock_clear_queue(mtx_clear_queue);
+				if (clear_thread_id.empty())
+				{
+					break;
+				}
+				else
+				{
+					thread_id = clear_thread_id.front();
+					clear_thread_id.pop();
+				}
+				ulock_clear_queue.unlock();
+				for (auto i = thread_pool.begin(); i != thread_pool.end(); i++)
+				{
+					if (i->get_id() == thread_id)
+					{
+						if (i->joinable())
+							i->join();
+						thread_pool.erase(i);
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
---
+++ Project1/ThreadPool.h
@@ -0,0 +1,56 @@
+#pragma once
+#include<thread>
+#include<string>
+#include<mutex>
+#include<condition_variable>
+#include<list>
+#include<queue>
+#include<atomic>
+#include<iostream>
+
+class ThreadTask
+{
+public:
+	virtual void start();
+};
+
+class ThreadPool
+{
+private:
+	int core_pool_size;
+	int max_pool_size;
+	int buffer_size;
+	int keep_alive_seconds;
+	std::atomic<int> running_num;
+	std::atomic<bool> termination_flag;
+	std::condition_variable cv_thread_pool,cv_management;
+	std::mutex mtx;
+	std::queue<ThreadTask*> task_buffer;
+	std::list<std::thread> thread_pool;
+	std::thread threadpool_management;
+
+	//处理多余线程需要的变量
+	int last_running_num;
+	std::atomic<bool> clear_flag;
+	std::atomic<int> need_clear_num;
+	std::chrono::time_point<std::chrono::high_resolution_clock> timestamp;
+	std::mutex mtx_clear_queue;
+	std::queue<std::thread::id> clear_thread_id;
+
+
+public:
+	//core_pool_size 常驻线程数； max_pool_size 线程池最大线程数；buffer_size 任务缓存数量，当任务缓存数量大于此设定值后线程池中线程数将增长；keep_alive_seconds 多于常驻线程数量时，多余线程的存活的最短时间
+	ThreadPool(int core_pool_size , int max_pool_size, int buffer_size, int  keep_alive_seconds);
+	~ThreadPool();
+	int poolSize();
+	int bufferSize();
+	void pushTask(ThreadTask* task);
+
+private:
+	ThreadPool(const ThreadPool& tp) = delete;
+	ThreadPool& operator()(const ThreadPool& tp) = delete;
+	void work();
+	void threadpoolManagement();
+
+};
+
